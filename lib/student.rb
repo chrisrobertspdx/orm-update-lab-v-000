93f501df942ebdb6567aa90975605d083cef7ae3
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-14 17:47:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,5 +58,10 @@ class Student
         self.new_from_db(r)
       }.first
   end
-  
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
 end
